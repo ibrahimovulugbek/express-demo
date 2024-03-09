84a381ba2bd7f47060feacb01a6824c26dfe448c
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-09 22:43:00 +0500

    build(database): connect mongodb

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const books = require('./routes/books')
 const home = require('./routes/home')
 const express = require('express');
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://localhost/test')
+    .then(() => {
+        console.log("Mongodb ga ulanish hosil bo'ldi...")
+    })
+    .catch((err) => {
+        console.log("Mongodb ga ulanib bo'lmadi!!!", err)
+    })
+ 
 const app = express();
 app.use(express.json())
+app.set('view engine', 'pug');
 
 app.use('/', home)
 app.use('/api/books', books)
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
