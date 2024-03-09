e9daaff2b7c828cb80865aa3182906541a55c0bf
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-09 18:02:24 +0500

    chore(books): deleted books array

--- books.js
+++
@@ -1,9 +0,0 @@
-const books = [
-    { id: 1, title: "Zukkolar va landavurlar" },
-    { id: 2, title: "Iroda kuchi" },
-    { id: 3, title: "Diqqat" },
-    { id: 4, title: "Atom odatlar" },
-    { id: 5, title: "Mafavvaqiyatli insonlarning 7 ko'nikmasi" }
-]
-
-module.exports = books
