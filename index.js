f72b56f6cbf1f2b25616b855b67d09b3e2047a17
ibrahimovulugbek <yuldoshova2002@gmail.com>
2024-03-12 16:43:28 +0500

    added customer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const books = require('./routes/books')
+const customers = require('./routes/customers')
 const home = require('./routes/home')
 const express = require('express');
 const mongoose = require('mongoose')
@@ -10,13 +11,14 @@ mongoose.connect('mongodb://localhost/test')
     .catch((err) => {
         console.log("Mongodb ga ulanib bo'lmadi!!!", err)
     })
- 
+
 const app = express();
 app.use(express.json())
 app.set('view engine', 'pug');
 
 app.use('/', home)
 app.use('/api/books', books)
+app.use('/api/customers', customers)
 
 
 const port = process.env.PORT || 5000
